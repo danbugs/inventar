c31fb83d360a63772e7f72fe393897aff6e45e6c
danbugs <danilochiarlone@gmail.com>
2021-07-30 16:11:00 +0200

    added recaptcha

--- inventar/src/Components/AuthForm/RegistrationForm.svelte
+++ inventar/src/Components/AuthForm/RegistrationForm.svelte
@@ -1,21 +1,47 @@
 <script>
-    export let isRegistered;
+  import { onMount, onDestroy } from "svelte";
+
+  export let isRegistered;
+
+  function verifyUser() {
+    document.getElementById("register-btn").removeAttribute("disabled");
+  }
+
+  onMount(() => {
+    window.verifyUser = verifyUser;
+  });
+
+  onDestroy(() => {
+    window.verifyUser = null;
+  });
 </script>
+
+<svelte:head>
+  <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+</svelte:head>
 
 <div class="h3">REGISTER</div>
 <hr />
 <form id="registerForm" on:submit|preventDefault>
   <div class="mb-3">
     <label class="form-label" for="user_name">Email:</label><br />
-    <input class="form-control" type="text" name="user_email" required/>
+    <input class="form-control" type="text" name="user_email" required />
     <label class="form-label" for="user_name">Username:</label><br />
-    <input class="form-control" type="text" name="user_name" required/>
-    <div class="form-text">The username must contain 3 or more characters, its' first character must be a alphabetic, and it may contain numbers and some symbols (.$!@-_)</div>
+    <input class="form-control" type="text" name="user_name" required />
+    <div class="form-text">
+      The username must contain 3 or more characters, its' first character must
+      be a alphabetic, and it may contain numbers and some symbols (.$!@-_)
+    </div>
     <label class="form-label" for="user_pwd">Password:</label><br />
-    <input class="form-control" type="password" name="user_pwd" required/>
-    <div class="form-text">The password must be between 8 to 15 characters, contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character.</div>
+    <input class="form-control" type="password" name="user_pwd" required />
+    <div class="form-text">
+      The password must be between 8 to 15 characters, contain at least one
+      lowercase letter, one uppercase letter, one numeric digit, and one special
+      character.
+    </div>
   </div>
-  <input class="btn btn-primary" type="submit" value="Register" />
+  <div class="g-recaptcha" data-sitekey="6LdT3M0bAAAAAJObLYDcRO7N4f4XXBbjBgMmcBRe" data-callback="verifyUser" />
+  <input id="register-btn" class="btn btn-primary" type="submit" value="Register" disabled/>
 </form>
 <hr />
 <div class="row justify-content-md-center">
@@ -23,9 +49,8 @@
     <div class="h5">Have an account?</div>
   </div>
   <div class="col-lg-3">
-    <button
-      class="btn btn-success"
-      on:click={() => (isRegistered = true)}>Login!</button
+    <button class="btn btn-success" on:click={() => (isRegistered = true)}
+      >Login!</button
     >
   </div>
 </div>
