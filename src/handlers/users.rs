3d4050ff43216358b5f6957c42ff93d697a5fee0
danbugs <danilochiarlone@gmail.com>
2021-07-31 22:09:25 +0200

    finished implementing email verify feature

--- src/handlers/users.rs
+++ src/handlers/users.rs
@@ -4,10 +4,13 @@ use inventar_lib::{
     models::users::{LoginUser, NewUser, RegisteringUser, User},
     schema::users::{self, dsl::*},
 };
-use rocket::http::Status;
+use rocket::{http::Status, response::content};
 use rocket_contrib::json::Json;
 
-use lettre::{message::{MultiPart, SinglePart, header}, transport::smtp::authentication::Credentials};
+use lettre::{
+    message::{header, MultiPart, SinglePart},
+    transport::smtp::authentication::Credentials,
+};
 use lettre::{Message, SmtpTransport, Transport};
 
 use dotenv::dotenv;
@@ -22,7 +25,11 @@ pub fn register(ru: Json<RegisteringUser>) -> Result<Status, Status> {
 
     // check if username already exists
     match users
-        .filter(user_name.eq(&nu.user_name).or(user_email.eq(&nu.user_email)))
+        .filter(
+            user_name
+                .eq(&nu.user_name)
+                .or(user_email.eq(&nu.user_email)),
+        )
         .first::<User>(&connection)
     {
         Ok(_) => {
@@ -31,7 +38,14 @@ pub fn register(ru: Json<RegisteringUser>) -> Result<Status, Status> {
         Err(_) => {}
     }
 
-    let verification_link = format!("http://localhost:8000/{}/{}", nu.user_name, nu.verification_code);
+    dotenv().ok();
+    
+    let host = env::var("HOST").expect("HOST must be set");
+
+    let verification_link = format!(
+        "{}/users/verify/{}/{}",
+        host, nu.user_name, nu.verification_code
+    );
 
     let html = html! {
         head {
@@ -50,52 +64,50 @@ pub fn register(ru: Json<RegisteringUser>) -> Result<Status, Status> {
     };
 
     let email = Message::builder()
-    .from("Inventar <donotreply.inventar@gmail.com>".parse().unwrap())
-    .to(nu.user_email.parse().unwrap())
-    .subject("Verify Your Account")
-    .multipart(
-        MultiPart::alternative()
-            .singlepart(
-                SinglePart::builder()
-                    .header(header::ContentType::TEXT_PLAIN)
-                    .body(String::from("Hello from Inventar!")),
-            )
-            .singlepart(
-                SinglePart::builder()
-                    .header(header::ContentType::TEXT_HTML)
-                    .body(html.into_string()),
-            ),
-    )
-    .expect("failed to build email");
+        .from("Inventar <donotreply.inventar@gmail.com>".parse().unwrap())
+        .to(nu.user_email.parse().unwrap())
+        .subject("Verify Your Account")
+        .multipart(
+            MultiPart::alternative()
+                .singlepart(
+                    SinglePart::builder()
+                        .header(header::ContentType::TEXT_PLAIN)
+                        .body(String::from("Hello from Inventar!")),
+                )
+                .singlepart(
+                    SinglePart::builder()
+                        .header(header::ContentType::TEXT_HTML)
+                        .body(html.into_string()),
+                ),
+        )
+        .expect("failed to build email");
 
     dotenv().ok();
-    let creds = Credentials::new(env::var("STMP_USERNAME").expect("STMP_USERNAME must be set"), env::var("STMP_PASSWORD").expect("STMP_PASSWORD must be set"));
-    
-    // Open a remote connection to gmail
+    let creds = Credentials::new(
+        env::var("STMP_USERNAME").expect("STMP_USERNAME must be set"),
+        env::var("STMP_PASSWORD").expect("STMP_PASSWORD must be set"),
+    );
+
     let mailer = SmtpTransport::relay("smtp.gmail.com")
         .unwrap()
         .credentials(creds)
         .build();
-    
-    // Send the email
+
     match mailer.send(&email) {
-        Ok(_) => {
-            diesel::insert_into(users::table)
+        Ok(_) => diesel::insert_into(users::table)
             .values(nu)
             .execute(&connection)
             .map(|_| Status::Created)
-            .map_err(|_| Status::InternalServerError)
-        },
+            .map_err(|_| Status::InternalServerError),
         Err(_) => Err(Status::InternalServerError),
     }
 }
 
-// block if not verified
 #[post("/login", data = "<lu>")]
 pub fn login(lu: Json<LoginUser>) -> Result<Status, Status> {
     let connection = establish_connection();
     match users
-        .filter(user_name.eq(&lu.user_name))
+        .filter(user_name.eq(&lu.user_name).and(verified_email.eq(true)))
         .first::<User>(&connection)
     {
         Ok(record) => {
@@ -109,14 +121,38 @@ pub fn login(lu: Json<LoginUser>) -> Result<Status, Status> {
     }
 }
 
-#[get("/verify/<uname>/<vcode>")]
-pub fn verify(uname : String, vcode : String) -> String{
+#[get("/verify/<uname>/<vcode>", format = "html")]
+pub fn verify(uname: String, vcode: String) -> content::Html<&'static str> {
     let connection = establish_connection();
+
+    let success_msg = content::Html(
+        r#"
+        <div style="font-family:Arial;text-align:center;">
+        <h1>You're verified!</h1>
+        <h3">Prooced to <a href="https://inventar.vercel.app">Inventar</a> to login!</h3>
+        </div>"#
+);
+
+    let error_msg = content::Html(
+        r#"
+        <div style="font-family:Arial;text-align:center;">
+        <h1>Something went wrong!</h1>
+        <h3>Try re-registering at <a href="https://inventar.vercel.app">Inventar</a>!</h3>
+        </div>"#
+    );
     match users
         .filter(user_name.eq(uname).and(verification_code.eq(vcode)))
         .first::<User>(&connection)
     {
-        Ok(_) => "You're Verified!".to_string(), // actually change verified field to true
-        Err(_) => "Failed verification".to_string()
+        Ok(user) => {
+            match diesel::update(users.filter(user_id.eq(user.user_id)))
+                .set(verified_email.eq(true))
+                .execute(&connection)
+            {
+                Ok(_) => success_msg,
+                Err(_) => error_msg
+            }
+        }
+        Err(_) => error_msg
     }
 }
